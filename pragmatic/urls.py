cf6545768481ecf1c9d7c4496056e92af1faf41b
Hyojung Nam <1326cy@gmail.com>
2024-03-27 12:14:30 +0900

    Delete redundant files

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('acount/', include('accountapp.urls'))
+    path('account/', include('accountapp.urls'))
 ]
