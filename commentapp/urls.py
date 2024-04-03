ee80f7cb90641141c57802d2708885289adad47f
Hyojung Nam <1326cy@gmail.com>
2024-04-03 12:03:10 +0900

    django course 38 commit

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
+
+
+
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
     # path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     # path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
-    # path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 ]
